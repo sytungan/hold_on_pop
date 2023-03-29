9e83eae7cfb21395adf0f5aa9a1a063f439a9cc0
Ankun <sytungan@gmail.com>
2023-03-29 09:55:26 +0700

    Remove unused import

--- test/hold_on_pop_test.dart
+++ test/hold_on_pop_test.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:hold_on_pop/hold_on_pop.dart';
-
 void main() {
-  test('adds one to input values', () {});
+  test('first test case', () {});
 }
